952fce05c11c7692649f35e2f292a2da3c8ca041
Liz <lizlarkin@highlandtechnology.com>
2020-11-04 21:00:32 -0800

    added data from API to current-weather div and added associated formatting

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 $(document).ready(function () {
 
 // Fetch Open Weather Map API
-var requestUrl = 'https://api.openweathermap.org/data/2.5/weather?q=London&appid=f47cf665982ed682ac53eda751512847'
+var requestUrl = 'https://api.openweathermap.org/data/2.5/weather?q=San Francisco&units=imperial&appid=f47cf665982ed682ac53eda751512847'
 
 function getApi(requestUrl) {
     fetch(requestUrl)
@@ -11,9 +11,48 @@ function getApi(requestUrl) {
       })
       .then(function (data) {
         console.log(data);
+        // Collect weather data that will be used in app
+        var currentTemp = data.main.temp;
+        // add date
+        var currentHumid = data.main.humidity;        
+        var weatherIconCode = data.weather[0].icon;
+        var weatherIcon = "http://openweathermap.org/img/w/" +  weatherIconCode + ".png";
+        var currentWind = data.wind.speed;
+
+        // Input Data into current-weather div
+
+        // City Name, Date, and Weather Icon
+        cityInfoEl = document.createElement('h2');
+        $("#current-weather").append(cityInfoEl);
+        cityInfoEl.textContent = "set city name somehow" + " date " + weatherIcon;
+
+        // Display city temperature
+        currentTempEl = document.createElement('div');
+        $("#current-weather").append(currentTempEl);
+        currentTempEl.textContent = (`Temperature:  ${currentTemp} \u00B0F`);
+
+        // Display city humidity
+        currentHumidEl = document.createElement('div');
+        $("#current-weather").append(currentHumidEl);
+        currentHumidEl.textContent = (`Humidity: ${currentHumid}%`);
+
+        // Display city wind speed
+        currentWindEl = document.createElement('div');
+        $("#current-weather").append(currentWindEl);
+        currentWindEl.textContent = (`Wind Speed: ${currentWind} MPH`);
+
+        // Display UV Index
+        currentUVEl = document.createElement('div');
+        $("#current-weather").append(currentUVEl);
+        currentUVEl.textContent = (`UV Index: `);
+
+        console.log(weatherIconCode);
       });
+      
   }
   
+  
+ 
 getApi(requestUrl);
 
 // Set starting key number for local storage
@@ -22,6 +61,7 @@ var keynum = localStorage.length + 1;
 // Search Button
 $("#search-button").on("click", function() {
     var city = $("#city-input").val();
+    console.log(city);
     localStorage.setItem(keynum, city);
     // increment key number each time button is clicked
     keynum = keynum + 1;
@@ -39,6 +79,13 @@ $("#search-button").on("click", function() {
 
     // Check that city searched can be found
 
-    // Retrieve weather data from city searched/clicked
+    // Push weather data to current-weather div
+
+
+    // Push 5-day forecast to forecast-weather div
+
+  // icons come from open weather API
+
+  // UV index color (green if favorable, etc.)
 
 });
