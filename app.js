3f5a2ad1730366a22f2be1638c6f9db37e3dfd8e
Liz <lizlarkin@highlandtechnology.com>
2020-11-05 12:13:55 -0800

    consolidated weather and forecast API calls into just forecast because it offers all of the same data and more

--- app.js
+++ app.js
@@ -1,9 +1,8 @@
 $(document).ready(function () {
 
 // Fetch Open Weather Map API
-var requestWeatherUrl = 'https://api.openweathermap.org/data/2.5/weather?q=Flin Flon&units=imperial&appid=f47cf665982ed682ac53eda751512847'
+var requestWeatherUrl = 'https://api.openweathermap.org/data/2.5/forecast?q=San Francisco&units=imperial&cnt=6&appid=f47cf665982ed682ac53eda751512847'
 var requestUVUrl = 'http://api.openweathermap.org/data/2.5/uvi?lat=37.7749&lon=122.4194&appid=f47cf665982ed682ac53eda751512847'
-var requestForecastUrl = 'https://api.openweathermap.org/data/2.5/forecast?q=San Francisco&units=imperial&cnt=6&appid=f47cf665982ed682ac53eda751512847'
 
 function getApi(requestWeatherUrl) {
     fetch(requestWeatherUrl)
@@ -15,19 +14,23 @@ function getApi(requestWeatherUrl) {
       .then(function (data) {
         console.log(data);
         // Collect weather data that will be used in app
-        var currentTemp = data.main.temp;
-        // add date
-        var currentHumid = data.main.humidity;        
-        var weatherIconCode = data.weather[0].icon;
+        var currentTemp = data.list[0].main.temp;
+        var date = data.list[0].dt_txt.split(" ")[0];
+        var currentHumid = data.list[0].main.humidity;        
+        var weatherIconCode = data.list[0].weather[0].icon;
         var weatherIconUrl = "http://openweathermap.org/img/wn/" +  weatherIconCode + "@2x.png";
-        var currentWind = data.wind.speed;
+        var currentWind = data.list[0].wind.speed;
+        var latitude = data.city.coord.lat;
+        console.log(latitude); // add this to requestUVUrl and remove
+        var longitude = data.city.coord.lon;
+        console.log(longitude); // add this to requestUVUrl and remove
   
         // Input Data into current-weather div
 
         // City Name, Date, and Weather Icon
         cityInfoEl = document.createElement('h2');
         $("#current-weather").append(cityInfoEl);
-        cityInfoEl.textContent = ("set city name somehow" + "date")
+        cityInfoEl.textContent = ("set city name somehow " + "(" + date + ")")
 
         weatherIconEl = document.createElement('img');
         weatherIconEl.setAttribute('src', weatherIconUrl);
@@ -53,7 +56,7 @@ function getApi(requestWeatherUrl) {
         $("#current-weather").append(currentUVEl);
         currentUVEl.textContent = (`UV Index: `);
 
-        console.log(weatherIconCode);
+        
       });
       
   }
@@ -67,26 +70,12 @@ function getApi(requestWeatherUrl) {
       })
       .then(function (data) {
         console.log(data);
-        var date = data.date_iso.split("T")[0];
-        console.log(date);
       })
     }
 
-    function getForecastApi(requestForecastUrl) {
-      fetch(requestForecastUrl)
-        .then(function (response) {
-            console.log(response.status);
-            return response.json()
-            
-        })
-        .then(function (data) {
-          console.log(data);
-        })
-      }
 
 getApi(requestWeatherUrl);
 getUVApi(requestUVUrl);
-getForecastApi(requestForecastUrl)
 
 // Set starting key number for local storage
 var keynum = localStorage.length + 1;
